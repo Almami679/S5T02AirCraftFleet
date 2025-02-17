00a54a2a80a0e8f29ccea112de4aade7e4b5af4b
Almami679 <almami989@gmail.com>
2025-02-17 20:40:49 +0100

    create firsts Microservices

---
+++ microservice-planes/src/test/java/com/microservice/planes/MicroservicePlanesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice.planes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicePlanesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
